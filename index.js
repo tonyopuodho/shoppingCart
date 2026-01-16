9cd89bcdb782092e5a2ea9589f3b83e043a86891
TONY OCHIENG <tonyopuodho@gmail.com>
2026-01-16 22:07:48 +0300

    fetching products from an api and displaying them on UI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const closeButton = document.querySelector(".close-btn")
 const openButton = document.querySelector(".open-btn")
 const mobileNavigation = document.querySelector(".mobile-navigation")
-
+const productContainer = document.querySelector(".product-container")
 openButton.addEventListener('click',() => {
     mobileNavigation.classList.add("active")
 })
@@ -9,3 +9,39 @@ openButton.addEventListener('click',() => {
 closeButton.addEventListener('click',() => {
     mobileNavigation.classList.remove("active")
 })
+
+/*
+ making an api call that displays products on the frontend side 
+*/
+
+
+let displayProducts = () => {
+    fetch('https://dummyjson.com/products',{method: "GET"})
+    .then((response) => response.json())
+    .then((data) => {
+        const products = data.products
+        productDetails(products)
+    })
+    .catch(error => console.log(error))
+}
+
+function productDetails(products) {
+    products.forEach((items) => {
+    const {id,title,price,images} = items
+    productContainer.innerHTML += `
+            <div class="products" id=${id} >
+                    <img src=${images} alt="products" class="product-img">
+                    <div class="product-details">
+                        <h3>${title}</h3>
+                        <div class="cash">
+                            <h2>Ksh ${price}</h2>
+                            <button class="add-btn">Add</button>
+                        </div>
+                    </div>
+            </div>
+        `
+    })
+  
+}
+
+displayProducts()
