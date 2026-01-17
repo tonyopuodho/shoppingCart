a611507750b08c4aa43b4b9baf4e765cacc1b921
TONY OCHIENG <tonyopuodho@gmail.com>
2026-01-17 19:24:09 +0300

    implementing add button functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const closeButton = document.querySelector(".close-btn")
 const openButton = document.querySelector(".open-btn")
 const mobileNavigation = document.querySelector(".mobile-navigation")
 const productContainer = document.querySelector(".product-container")
+const alertElement = document.querySelector(".alert")
+
 openButton.addEventListener('click',() => {
     mobileNavigation.classList.add("active")
 })
@@ -29,19 +31,37 @@ function productDetails(products) {
     products.forEach((items) => {
     const {id,title,price,images} = items
     productContainer.innerHTML += `
-            <div class="products" id=${id} >
+            <div class="products">
                     <img src=${images} alt="products" class="product-img">
                     <div class="product-details">
                         <h3>${title}</h3>
                         <div class="cash">
                             <h2>Ksh ${price}</h2>
-                            <button class="add-btn">Add</button>
+                            <button class="add-btn" onclick="addItem(${id})">Add</button>
                         </div>
                     </div>
             </div>
         `
     })
   
+}
+/**
+ * 
+ * adding items to cart 
+ */
+
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+function addItem(id){
+    alertElement.classList.add("active")
+    setTimeout(() => {
+        alertElement.classList.remove("active")
+    },1000)
+    cart.push({
+        id: id
+    })
+
+    localStorage.setItem("cart",JSON.stringify(cart))
 }
 
 displayProducts()
